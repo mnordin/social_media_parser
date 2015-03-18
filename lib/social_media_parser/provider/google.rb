1df8c394effa9e578e39bd027bcb623d9a91eb9e
Markus Nordin <markus.nordin@gmail.com>
2015-03-18 23:58:29 +0100

    More readable google plus url builder

--- lib/social_media_parser/provider/google.rb
+++ lib/social_media_parser/provider/google.rb
@@ -5,21 +5,24 @@ module SocialMediaParser
     class Google < Base
       URL_REGEX = /(?:(?:http|https):\/\/)plus.google.com\/?(?:u\/\d{1,}\/|)(?:\+|)([\w\-\.\%]{1,})/i
 
+      # Google Plus urls prepends + for username based urls and without for id based
       def url
         return extract_url_from_attributes if extract_url_from_attributes
+
         if username
-          if Float(username)
-            "https://plus.google.com/#{username}"
-          end
+          url_username = is_i?(username) ? "+#{username}" : username
+          "https://plus.google.com/#{url_username}"
         end
-      rescue ArgumentError
-        "https://plus.google.com/+#{username}"
       end
 
       private
 
       def extract_username_from_url
         URL_REGEX.match(extract_url_from_attributes).to_a[1]
+      end
+
+      def is_i?(string)
+        !string.match(/\A\d+\z/)
       end
     end
   end
